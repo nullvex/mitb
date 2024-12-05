4647b50872f1f282d83f6656a31090a88e5d4c08
nvx <nvx@j03l.com>
2024-12-04 22:09:42 -0800

    latest

--- bash/launchTestMitbLXDInstance.sh
+++ bash/launchTestMitbLXDInstance.sh
@@ -11,7 +11,9 @@ cat mitb.lxd.profile.yaml | lxc profile edit mitb
 lxc launch ubuntu:24.04 $HOST -p mitb
 cd /home/jj/.config/
 tar --exclude logs -jcvf /tmp/gcloud.tar.bz2 gcloud
-lxc file push /tmp/gcloud.tar.bz2 mitbs/root/gcloud.tar.bz2
-lxc file push /home/jj/.ssh/id_rsa mitbs/root/.ssh/
-lxc file push /home/jj/.ssh/id_rsa.pub mitbs/root/.ssh/
+tar -jcvf /tmp/vim.tar.bz2 /home/jj/.vim /home/jj/.vimrc
+lxc file push /tmp/gcloud.tar.bz2 $HOST/root/gcloud.tar.bz2
+lxc file push /tmp/vim.tar.bz2 $HOST/root/vim.tar.bz2
+lxc file push /home/jj/.ssh/id_rsa $HOST/root/.ssh/
+lxc file push /home/jj/.ssh/id_rsa.pub $HOST/root/.ssh/
 
