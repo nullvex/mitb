9e0447b3168cb808c0a4760eec891fbec5874f5a
nvx <nvx@j03l.com>
2024-12-04 20:48:24 -0800

    cleaning up mitbs

--- mitbs/bash/launchTestMitbLXDInstance.sh
+++ mitbs/bash/launchTestMitbLXDInstance.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
+if [ -z "$1" ]
+  then
+    echo "No Host argument supplied"
+  else
+    HOST=$1
+fi
 pubkey=`cat /home/jj/.ssh/id_rsa.pub`
 privatekey=`cat /home/jj/.ssh/id_rsa`
 cat mitb.lxd.profile.yaml | lxc profile edit mitb
-lxc launch ubuntu:24.04 mitbs -p mitb
+lxc launch ubuntu:24.04 $HOST -p mitb
 cd /home/jj/.config/
 tar --exclude logs -jcvf /tmp/gcloud.tar.bz2 gcloud
 lxc file push /tmp/gcloud.tar.bz2 mitbs/root/gcloud.tar.bz2
